ee97456fd37d4ab7f36bd75d71d815e828a4b628
Marcos Garcia <143760248+marcdevelopez@users.noreply.github.com>
2024-05-07 19:54:37 +0200

    conseguido enlazar la actividad de autenticacion con la del home de cliente al realizar registro por correo y contraseña de firebase con exito.

--- app/src/main/java/com/marcdevelopez/tindnet/HomeClientActivity.java
+++ app/src/main/java/com/marcdevelopez/tindnet/HomeClientActivity.java
@@ -2,16 +2,14 @@ package com.marcdevelopez.tindnet;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.marcdevelopez.tindnet.databinding.ActivityHomeClientBinding;
-
 
 public class HomeClientActivity extends AppCompatActivity {
 
@@ -24,15 +22,15 @@ public class HomeClientActivity extends AppCompatActivity {
         binding = ActivityHomeClientBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar); // Configura la ActionBar
+
+        // Configura la navegación con el NavController
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_home_client);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_home_client);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
 }
