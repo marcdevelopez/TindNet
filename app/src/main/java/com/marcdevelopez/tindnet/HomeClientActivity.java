628cfb1cc70157957828309e29634924a2a7275e
Marcos Garcia <143760248+marcdevelopez@users.noreply.github.com>
2024-05-09 20:42:11 +0200

    renombrar layout principal de authactivity a authlayout, para poder hacer invisible si hay sesión iniciada.

--- app/src/main/java/com/marcdevelopez/tindnet/HomeClientActivity.java
+++ app/src/main/java/com/marcdevelopez/tindnet/HomeClientActivity.java
@@ -1,5 +1,7 @@
 package com.marcdevelopez.tindnet;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +12,37 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.marcdevelopez.tindnet.databinding.ActivityHomeClientBinding;
+import com.marcdevelopez.tindnet.provider.ProviderType;
+import com.marcdevelopez.tindnet.util.Constants;
 
 public class HomeClientActivity extends AppCompatActivity {
 
+    private String email;
+    private ProviderType provider;
     private ActivityHomeClientBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // recupero email y provider de AuthActivity
+        Bundle bundle = this.getIntent().getExtras();
+        // validamos que no está vacio el bundle
+        if (bundle != null) {
+            email = bundle.getString(Constants.EMAIL_AUTH);
+            provider = (ProviderType) bundle.get(Constants.PROVIDER);
+        }
+
+        // guardamos datos de email y provider en inicio de sesión para usar en caso de cierre de aplicación
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("email", email);
+        editor.putString("provider", provider.toString());
+        editor.apply();
+
+        /**TODO: en menú superior del home, en cerrar sesión, llamaremos al sharedprerferences
+         * R.string.prefs_file y llamaremos a clear() para eliminar estos datos
+          */
+
 
         binding = ActivityHomeClientBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
