dee25847abb87d0d066b0360450391d199655b81
Marcos Garcia <143760248+marcdevelopez@users.noreply.github.com>
2024-05-10 12:39:02 +0200

    implementacion completa de autenticacion con google sin exito: creados metodos signInWithGoogle, firebaseAuthWithGoogle y sobreescrito onActivityResult.

--- app/src/main/java/com/marcdevelopez/tindnet/AuthActivity.java
+++ app/src/main/java/com/marcdevelopez/tindnet/AuthActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -16,7 +15,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 
-
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -25,12 +24,25 @@ import com.google.firebase.auth.FirebaseUser;
 import com.marcdevelopez.tindnet.provider.ProviderType;
 import com.marcdevelopez.tindnet.util.Constants;
 
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.*;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 public class AuthActivity extends AppCompatActivity {
 
+    // para cuenta de google:
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleSignInClient mGoogleSignInClient;
     private static FirebaseAuth mAuth;
+
     private EditText mEmailRegisterEditText, mPasswordEditText;
-    private Button mRegisterWithPassword;
-    private RadioGroup mRadioGroup;
+    private Button buttonRegisterWithPassword;
+    private Button buttonRegisterWithGoogle;
+    private RadioGroup radioGroupClientCompany;
     private boolean soyCliente;
     // necesario provider para saber como se autenticó el usuario en la home (google, correo y contraseña, facebook...):
     private ProviderType provider;
@@ -42,8 +54,9 @@ public class AuthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_auth);
         mEmailRegisterEditText = findViewById(R.id.etEmailRegister);
         mPasswordEditText = findViewById(R.id.etPassword);
-        mRegisterWithPassword = findViewById(R.id.buttonRegister);
-        mRadioGroup = findViewById(R.id.radioGroupRegisterEmpClient);
+        buttonRegisterWithPassword = findViewById(R.id.buttonRegister);
+        buttonRegisterWithGoogle= findViewById(R.id.imageButtonGoogle);
+        radioGroupClientCompany = findViewById(R.id.radioGroupRegisterEmpClient);
         // necesario para saber si está logeado usuario y para crear usuario nuevo.
         mAuth = FirebaseAuth.getInstance();
         // hace el login en general escuchando los botones de login y la elección de tipo de usuario
@@ -54,7 +67,7 @@ public class AuthActivity extends AppCompatActivity {
 
     private void setup() {
         // guardamos la selección de usuario Cliente o Empresa
-        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        radioGroupClientCompany.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 if (checkedId == R.id.radioButtonCliente) {
@@ -64,7 +77,7 @@ public class AuthActivity extends AppCompatActivity {
         });
 
         // autenticamos los datos para inicio de sesión con contraseña
-        mRegisterWithPassword.setOnClickListener(new View.OnClickListener() {
+        buttonRegisterWithPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = mEmailRegisterEditText.getText().toString();
@@ -93,7 +106,71 @@ public class AuthActivity extends AppCompatActivity {
             }
         });
 
+        // autenticamos los datos para inicio de sesión con Google
+        buttonRegisterWithGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Configura el cliente de inicio de sesión con Google
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build();
 
+                mGoogleSignInClient = GoogleSignIn.getClient(AuthActivity.this, gso);
+                mAuth = FirebaseAuth.getInstance();
+
+                // Comprueba si el usuario ya ha iniciado sesión
+                if (mAuth.getCurrentUser() != null) {
+                    // El usuario ya ha iniciado sesión, puedes redirigir a la siguiente actividad
+                    // o realizar cualquier acción que necesites aquí
+                } else {
+                    // El usuario no ha iniciado sesión, muestra el botón de inicio de sesión con Google
+                    signInWithGoogle();
+                }
+            }
+        });
+
+    }
+
+    private void signInWithGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Resultado retornado por el inicio de sesión con Google
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google inicio de sesión exitoso, autentica con Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google inicio de sesión fallido, muestra un mensaje al usuario
+                Toast.makeText(this, "Inicio de sesión con Google fallido", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Inicio de sesión con Google exitoso
+                            // Puedes obtener el usuario actual y redirigir a la siguiente actividad aquí
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // Inicio de sesión con Google fallido, muestra un mensaje al usuario
+                            Toast.makeText(AuthActivity.this, "Autenticación con Firebase fallida", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private void session() {
