3ccec96220b3b8e7a9cd2f381e06a24d4db0ba49
Marcos Garcia <143760248+marcdevelopez@users.noreply.github.com>
2024-05-16 11:23:03 +0200

    configuracion del nav_home_client para que este con los id del bottomnavigation. creacion del metodo initNavigation en HomeClientActivity para que se inicie en el oncreate el menu inferior de navegacion.

--- app/src/main/java/com/marcdevelopez/tindnet/HomeClientActivity.java
+++ app/src/main/java/com/marcdevelopez/tindnet/HomeClientActivity.java
@@ -1,14 +1,37 @@
 package com.marcdevelopez.tindnet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.marcdevelopez.tindnet.databinding.ActivityHomeClientBinding;
+
 public class HomeClientActivity extends AppCompatActivity {
+
+    private ActivityHomeClientBinding binding;
+    private NavHostFragment navHostFragment;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_client);
+        binding = ActivityHomeClientBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initNavigation();
+
     }
+
+    private void initNavigation() {
+        // se referencia o instancia con
+        navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_container);
+        navController = navHostFragment.getNavController();
+        // pasamos el id de nuestro bottonnavigation del xml de la actividad
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+    }
+
 }
